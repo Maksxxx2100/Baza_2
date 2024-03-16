2ea85b60a205691e8b69ade1beeaedf0a0d1f8a7
Maksxxx2100 <maksxxx2100@mail.ru>
2024-03-16 17:07:48 +0500

    add tables: Actor and Film

--- DATAbase/Film.py
+++ DATAbase/Film.py
@@ -1,4 +1,15 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String,Date
 from sqlalchemy.orm import relationship
 
 from base_meta import Base
+
+class Film(Base):
+    __tablename__ = "Film"
+
+    film_id = Column(Integer, primary_key=True, autoincrement=True)
+    film_title = Column(String(50), nullable=False)
+    film_duration = Column(Integer)
+    film_release_date = Column(Date)
+
+    def __repr__(self):
+        return f"Film(id={self.id!r}, title={self.film_title!r}, duration = {self.film_duration!r},date={self.film_release_date})"
