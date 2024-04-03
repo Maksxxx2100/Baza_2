cf17c5dd49b2580c9966bba5c70ee0b7aa7403ba
Maksxxx2100 <maksxxx2100@mail.ru>
2024-04-03 16:10:38 +0500

    add migration, postgres

--- DATAbase/__init__.py
+++ DATAbase/__init__.py
@@ -1,1 +1,17 @@
 from .base_meta import Base, get_session
+from .Category  import Category
+from .Film import Film
+from .Actor import Actor              # передаем классы
+from .Film_Actor import Film_Actor
+from .Film_Filmmaker import Film_Filmmaker
+from .Film_Review import Film_Review
+from .Filmmaker import Filmmaker
+from .Review import Review
+from .Serial import Serial
+from .Serial_Actor import Serial_Actor
+from .Serial_Review import Serial_Review
+from .User import User
+from .User_Film import User_Film
+from .Serial_Filmmaker import Serial_Filmmaker
+from .User_Review import User_Review
+from .User_Serial import User_Serial
--- DATAbase/base_meta.py
+++ DATAbase/base_meta.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-engine = create_engine("sqlite:///../DB_2.db", echo=True)
+from Config import DB_HOST, DB_PORT, DB_PASSWORD, DB_USER, DB_NAME
+
+engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}", echo=False)
 Base = DeclarativeBase()
 get_session = sessionmaker(engine, autocommit = False)
